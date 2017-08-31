f7fbda04a865b977bec71fb007f39afd5e3f006d
ASUS <ASUS@asus-pc>
2017-08-31 22:24:17 +0800

    initial version

---
+++ src/main/java/com/autoinjecttest/spring_boot_autoinject_test/App.java
@@ -0,0 +1,28 @@
+package com.autoinjecttest.spring_boot_autoinject_test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.autoinject.spring_boot_autoinject.HelloService;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@SpringBootApplication
+public class App 
+{
+   @Autowired
+   private HelloService helloService;
+   
+   public String index() {
+	   return helloService.sayHello();
+   }
+   
+   public static void main(String[] args) {
+	   SpringApplication.run(App.class,args);
+   }
+}
