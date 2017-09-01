1302ce6b04632d7b1a1d03390a3e2dfd049e3f9a
mhuan <matt.xiaoyue.huang@kingland.com>
2017-09-01 15:24:55 +0800

    fix auto configure

--- src/main/java/com/autoinjecttest/spring_boot_autoinject_test/App.java
+++ src/main/java/com/autoinjecttest/spring_boot_autoinject_test/App.java
@@ -3,6 +3,7 @@ package com.autoinjecttest.spring_boot_autoinject_test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.autoinject.spring_boot_autoinject.HelloService;
@@ -18,6 +19,7 @@ public class App
    @Autowired
    private HelloService helloService;
    
+   @RequestMapping("/")
    public String index() {
 	   return helloService.sayHello();
    }
